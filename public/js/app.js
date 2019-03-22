0ca8c4f348259151f437dd4b8edb79e9b5cef620
cc0a <jmckeeco84@gmail.com>
2019-03-21 21:19:11 -0400

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-one'); // Used for loading message and errors
+const messageTwo = document.querySelector('#message-two');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault(); // Prevents the browser from resetting the page!
+
+    const location = search.value;
+
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+    // fetch is being used to retrieve JSON data clientside, which in taps into server-side JS which taps into external API
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+            }
+        })
+    });
+});
+
+
+
+
+
+
---
+++ src/app.js
@@ -0,0 +1,101 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+const app = express();
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'James McKee',
+        copyright: '2019 cc0a'
+    })
+});
+
+app.get('/help', (req, res) => {
+   res.render('help', {
+       helpText:'This is some helpful TEXT!',
+       title: 'Help',
+       name: 'James McKee',
+       copyright: '2019 cc0a'
+   });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'James McKee',
+        copyright: '2019 cc0a'
+    })
+});
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address) {
+        return res.send({
+            error: 'Please provide an address'
+        });
+    }
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => { // Setting up a default object in this case prevents the server from crashing, because it covers all possible bad searches
+       if(error) {
+           return res.send({ error })
+       }
+       forecast(latitude, longitude, (error, forecastData) => {
+          if(error) {
+              return res.send({ error })
+          }
+          res.send({
+              forecast: forecastData,
+              location,
+              address: req.query.address
+          });
+       });
+    });
+});
+
+app.get('/help/*', (req, res) => {
+   res.render('helpmissing', {
+       title: '404 Page',
+       helpMissing: 'Help Article not Found',
+       copyright: '2019 cc0a'
+   });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        prompt: '404: Page Not Found',
+        copyright: '2019 cc0a'
+    });
+});
+
+// app.com
+// app.com/help
+// app.com/about
+
+app.listen(3000, () => { // First param is the port being specified, second param is a callback that runs once the server is functional
+    console.log('Server is up on port 3000.');
+});
+
+
+
+
+
+
+
+
+
